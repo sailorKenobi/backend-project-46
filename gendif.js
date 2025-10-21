77410389089a4db0c5cb438d19946c4a3bbc4e25
Max <38960498+sailorKenobi@users.noreply.github.com>
2025-10-21 20:35:32 +0300

    step2: add some arguments

--- gendif.js
+++ gendif.js
@@ -1,3 +1,4 @@
+//https://github.com/tj/commander.js?tab=readme-ov-file#automated-help
 import { Command } from "commander"
 
 const genDiff = (filepath1, filepath2) => {
@@ -7,10 +8,14 @@ const genDiff = (filepath1, filepath2) => {
 export default genDiff
 
 const program = new Command()
+program.description('Compares two configuration files and shows a difference.')
+program.argument('<filepath1>')
+program.argument('<filepath2>')
 program.option('-V, --version', 'output the version number')
-program.option('-h, --help', 'display help for command')
+program.option('-f, --format [type]', 'output format')
+
 program.parse(process.argv)
 
-const options = program.opts()
-if (options.help)
-    console.log(program.helpInformation())
+//const options = program.opts()
+//if (options.help)
+//    console.log(program.helpInformation())
